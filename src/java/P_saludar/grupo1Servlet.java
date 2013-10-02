71baeb76e1a3325335ebd22550ed820a43837eaf
al037213 <al037213@s09-pc19>
2013-10-02 10:41:02 +0200

    commit actualizacion salva.jsp

--- src/java/P_saludar/grupo1Servlet.java
+++ src/java/P_saludar/grupo1Servlet.java
@@ -41,7 +41,7 @@ public class grupo1Servlet extends HttpServlet {
             out.println("<body>");
             out.println("<h1>Servlet grupo1Servlet at " + request.getContextPath() + "</h1>");
             out.println("<a href='../01-Holamundo/Jorge.jsp'><h1>Jorge.jsp </h1></a>");
-            out.println("<a href='../01-Holamundo/Salva.jsp'><h1>salva.jsp </h1></a>");
+            out.println("<a href='../01-Holamundo/salva.jsp'><h1>salva.jsp </h1></a>");
             out.println("<a href='../01-Holamundo/cristina.jsp'><h1>cristina.jsp </h1></a>");
             out.println("<a href='../01-Holamundo/juan.jsp'><h1>juan.jsp </h1></a>");
             out.println("</body>");
